0a964910c933fba0ca34d35948435ceb68c0b59b
Jay stones <Joelpaul345@gmail.com>
2023-06-28 01:45:27 +0100

    :broom: Psycodestyle Compliant

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,7 @@
 
 def pascal_triangle(n):
     '''Creates a list of lists of integers representing
-    the Pascal's triangle of a given integer value.
+    the Pascal's triangle of a given integer.
     '''
     if n <= 0:
         return []
