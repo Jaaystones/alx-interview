e128a85cbf0c1f9e2dec6bc8deb4c789c2f6ad5e
Jay stones <Joelpaul345@gmail.com>
2023-07-11 13:49:30 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,17 +6,32 @@ You must imagine that there is only one function
 to copy everything and one to paste.
 '''
 
+import math
 
-def minOperations(n):
-    p = 0
 
-    if n <= 1:
-        return p
+def min_operations(n):
+    """
+    Calculates the fewest number of operations needed to achieve exactly n 'H' characters in a text file.
 
-    for i in range(2, n + 1):
-        while (0 == n % i):
-            p = p + i
-            n = n / i
-            if n < i:
-                break
-    return p
+    Args:
+        n (int): The desired number of 'H' characters.
+
+    Returns:
+        int: The fewest number of operations needed. Returns 0 if it is impossible to achieve.
+
+    """
+    if n == 1:
+        return 0
+
+    # Finding the smallest factor of n
+    smallest_factor = None
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            smallest_factor = i
+            break
+
+    if smallest_factor is None:
+        return n
+
+    # Perform "Copy All" once and "Paste" smallest_factor - 1 times
+    return 1 + min_operations(n // smallest_factor)
