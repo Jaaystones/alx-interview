6d6ac2e53cd7fdd997c64da837cc07b49a544565
Jay stones <Joelpaul345@gmail.com>
2023-07-11 14:17:08 +0100

    :white_check_mark: pycodestyle compliant

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,30 +6,35 @@
 def minOperations(n):
     '''Computes the fewest number of operations needed to result
     in exactly n H characters.
+
+    Args:
+        n (int): The desired number of H characters.
+
+    Returns:
+        int: The minimum number of operations required.
+
     '''
     if not isinstance(n, int):
         return 0
     no_of_ops = 0
     clipboard = 0
-    paste = 1
+    done = 1
     # print('H', end='')
-    while paste < n:
+    while done < n:
         if clipboard == 0:
             # init (the first copy all and paste)
-            clipboard = paste
-            paste += clipboard
+            clipboard = done
+            done += clipboard
             no_of_ops += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif n - paste > 0 and (n - paste) % paste == 0:
+        elif n - done > 0 and (n - done) % done == 0:
             # copy all and paste
-            clipboard = paste
-            paste += clipboard
-           no_of_ops += 2
-            # print('-(11)->{}'.format('H' * paste), end='')
+            clipboard = done
+            done += clipboard
+            no_of_ops += 2
         elif clipboard > 0:
             # paste
-            paste += clipboard
+            done += clipboard
             no_of_ops += 1
-            # print('-(01)->{}'.format('H' * paste), end='')
+            # print('-(01)->{}'.format('H' * done), end='')
     # print('')
     return no_of_ops
