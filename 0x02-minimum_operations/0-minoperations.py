7211f3357d8096046fd8a706ea6166ba80cb5099
Jay stones <Joelpaul345@gmail.com>
2023-07-11 14:11:21 +0100

    :wrench:Fixed pycodestyle and code matrix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,27 +9,27 @@ def minOperations(n):
     '''
     if not isinstance(n, int):
         return 0
-    ops_count = 0
+    no_of_ops = 0
     clipboard = 0
-    done = 1
+    paste = 1
     # print('H', end='')
     while done < n:
         if clipboard == 0:
             # init (the first copy all and paste)
-            clipboard = done
-            done += clipboard
-            ops_count += 2
+            clipboard = paste
+            paste += clipboard
+            no_of_ops += 2
             # print('-(11)->{}'.format('H' * done), end='')
-        elif n - done > 0 and (n - done) % done == 0:
+        elif n - paste > 0 and (n - paste) % paste == 0:
             # copy all and paste
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
+            clipboard = paste
+            paste += clipboard
+           no_of_ops += 2
+            # print('-(11)->{}'.format('H' * paste), end='')
         elif clipboard > 0:
             # paste
-            done += clipboard
-            ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
+            paste += clipboard
+            no_of_ops += 1
+            # print('-(01)->{}'.format('H' * paste), end='')
     # print('')
-    return ops_count
+    return no_of_ops
